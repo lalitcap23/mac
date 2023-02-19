3f6e3bd4a708c7c3ceca7cab04c68cf67f5dcf9c
lalitcap23 <lalitrajput232002>
2023-02-19 13:40:37 +0530

    new file

---
+++ modulo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+     int a ,b;
+    // printf("Give the first number :");
+    // scanf("%d", &a);
+    // printf("Give the second number(it must be small) :");
+    // scanf("%d", &b);
+    // int r =a%b;// it is the modulo function which is used to find the remainder of any two numerbers.[a%b=a always when a<b]
+    // printf ("your answer %d",r);
+
+    // float x;
+    // printf ("enter the inter :");
+    // scanf("%f",&x);
+    // int y=x;
+    // float a= x-y;
+    // printf("The answer is : %f",a );
+    char lalit='A';
+    printf("Thor's brother is %d", lalit);
+
+    
+ scanf("%d", &a);
+
+
+
+       return 0;
+  
+}
